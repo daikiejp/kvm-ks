aa2f060b413062bbcb26c48cdc68a26c4ee30aea
Zhang Dan Jie <dannyec@users.noreply.github.com>
2018-04-17 23:28:43 +0800

    Create centos7.sh

---
+++ centos7.sh
@@ -0,0 +1,11 @@
+virt-install \
+--name vps1 \
+--network bridge=br0 \
+--ram=2048 \
+--os-type linux --os-variant=rhel7 \
+--vcpus=1 \
+--nographics \ 
+--location='https://mirrors.tuna.tsinghua.edu.cn/centos/7.4.1708/os/x86_64/' \
+--initrd-inject=/root/centos7x_kvm.ks
+--extra-args='ks=file:/centos7x_kvm.ks text console=tty0 console=ttyS0,115200n8 serial' \
+--disk path=/vm-images/vps1.img,size=12 --hvm
